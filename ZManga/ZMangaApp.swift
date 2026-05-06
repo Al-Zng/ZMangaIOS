aa710bb1b20d1de45a93ff6b02a1ff3050b29f31
Al-Zng <mohammedznger@gmail.com>
2026-05-06 22:59:03 +0300

    Update ZMangaApp.swift

--- ZManga/ZMangaApp.swift
+++ ZManga/ZMangaApp.swift
@@ -4,12 +4,14 @@ import SwiftUI
 struct ZMangaApp: App {
     @StateObject private var store = AppStore()
     @StateObject private var network = NetworkMonitor.shared
+    @StateObject private var logger = Logger.shared   // أضف هذا السطر
 
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .environmentObject(store)
                 .environmentObject(network)
+                .environmentObject(logger)            // حقنه
                 .preferredColorScheme(.dark)
         }
     }
